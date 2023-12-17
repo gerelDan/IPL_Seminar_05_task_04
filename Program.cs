3ee636aa90cef1e6ee0dffd566927c0ac9aeb400
gerel <gerel@bk.ru>
2023-12-17 18:43:38 +0500

    Final

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ int[,] FillArray(int rows, int columns, int minValue, int maxValue)
     {
         for (int j = 0; j < result.GetLength(1); j++)
         {
-            result[i,j] = new Random().Next(minValue, maxValue+1);
+            result[i,j] = new Random().Next(minValue, maxValue);
         }
     }
     return result;
@@ -35,3 +35,45 @@ int[] FindMinIndex(int[,] arr)
     }
     return result;
 }
+int[,] DelRowColumn(int[,] arr, int row, int column)
+{
+    int[,] result = new int[arr.GetLength(0)-1, arr.GetLength(1)-1];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            if (i>= row)
+            {
+                if (j>=column)
+                {
+                    result[i,j] = arr[i+1, j+1];
+                }
+                else result[i,j] = arr[i+1, j];
+            }
+            else if (j>=column) result[i,j] = arr[i, j+1];
+            else result[i, j] = arr[i, j];
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == arr.GetLength(1)-1)
+            {
+                Console.WriteLine($"{arr[i,j]}]");
+            }
+            else Console.Write($"{arr[i,j]}, ");
+        }
+    }
+}
+int[,] array = FillArray(5, 5, 10, 100);
+PrintArray(array);
+int[] minIn = FindMinIndex(array);
+Console.WriteLine();
+int[,] resArray = DelRowColumn(array, minIn[0], minIn[1]);
+PrintArray(resArray);
