c150684a43461e87cccb30d94f53ad5a0fc03faf
gerel <gerel@bk.ru>
2023-12-17 15:17:38 +0500

    Add fillArray

--- Program.cs
+++ Program.cs
@@ -4,4 +4,17 @@
 //на пересечении которых расположен наименьший элемент массива.
 //Под удалением понимается создание нового двумерного массива
 // без строки и столбца
+using System.ComponentModel.DataAnnotations;
 
+int[,] FillArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue+1);
+        }
+    }
+    return result;
+}
